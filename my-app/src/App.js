453ecd3f5b12ddb24d7f59debfe513a2a907d302
Unknown <ctcook97@gmail.com>
2018-02-25 11:35:48 -0500

    Forgot this stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import '../App.css';
+import './App.css';
 import { connect } from 'react-redux';
-import { addOrder } from '../actions';
+import { addOrder } from './actions';
 
 class App extends Component {
   constructor(props) {
