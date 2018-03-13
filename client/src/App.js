9062679376acf4dd2616eab033ac2356ef9f34a8
James Fernandes <jtfernandes99@gmail.com>
2018-03-13 14:45:49 -0400

    Used bootstrap container since material-ui does not support

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
-import {GridList} from 'material-ui/GridList';
+import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
@@ -13,9 +13,9 @@ import OrderCards from './components/OrderCards';
   Bootstrap UI implementation
 */
 
-/*
-import './App.css';
 
+import './App.css';
+/*
 const App = ({
   addOrder,
   onlineOrders,
@@ -67,19 +67,6 @@ const App = ({
 )
 */
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'row wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-    overflowY: 'auto',
-  },
-};
-
 
 const App = ({
   addOrder,
@@ -93,19 +80,46 @@ const App = ({
     iconClassNameRight="muidocs-icon-navigation-expand-more"
     />
 
-    <div style={styles.root}>
-      <GridList
-        cellHeight={180}
-        style={styles.gridList}
-      >
-        <Subheader>Online</Subheader>
-        <OrderCards orders={onlineOrders} />
-      </GridList>
+    <div className="App">
+      <div className="sub-title">
+        <div className="container">
+          <div className="row sections">
+            <div className="col-sm col1 rounded">
+              <h4>Online</h4>
+              <div className="container">
+                <div className="row sub-sections">
+                  <div className="col-xs orders">
+                    <OrderCards orders={onlineOrders} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm col2 rounded">
+              <h4>In-store</h4>
+              <div className="container">
+                <div className="row sub-sections">
+                  <div className="col-xs orders">
+                    <OrderCards orders={instoreOrders} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm col3 rounded">
+              <h4>Pickup</h4>
+              <div className="container">
+                <div className="row sub-sections">
+                  <div className="col-xs orders">
+                    <OrderCards orders={pickupOrders} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
-
   </MuiThemeProvider>
-
 )
 
 const mapStateToProps = (state, originalProps) => ({
